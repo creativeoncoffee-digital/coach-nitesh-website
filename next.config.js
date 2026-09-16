240566fd50f17e4e5444499516dfb754bdf0dd0d
creativeoncoffee-digital <coc.webdevelopment@gmail.com>
2026-09-16 20:05:20 +0530

    update the next.config file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  // Yeh line Next.js ko 'out' folder banane ka instruction deti hai
+  output: 'export', 
+  
+  images: {
+    // Static export ke time Next.js ka default image optimizer kaam nahi karta
+    // Isko true karna zaroori hai kyunki humne Hero section mein <Image /> component use kiya hai
+    unoptimized: true, 
+  }
 };
 
-export default nextConfig;
+module.exports = nextConfig;
